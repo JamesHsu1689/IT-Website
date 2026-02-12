4dd2ba481e79d1e91ee2569f4d1af19bf2892b1b
JamesHsu1689 <james.ca.hsu@outlook.com>
2026-02-11 20:37:22 -0700

    Stylized navbar and branding

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector(".site-navbar");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 10) {
+            navbar.classList.add("nav-scrolled");
+        } else {
+            navbar.classList.remove("nav-scrolled");
+        }
+    });
+});
